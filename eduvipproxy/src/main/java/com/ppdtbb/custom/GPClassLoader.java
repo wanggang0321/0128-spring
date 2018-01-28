f456806844297c5893c74ff37c6e1a5cd0a4ee4e
wanggang0302 <wanggangjava@126.com>
2018-01-28 22:46:11 +0800

    1.手写动态代理

--- eduvipproxy/src/main/java/com/ppdtbb/custom/GPClassLoader.java
+++ eduvipproxy/src/main/java/com/ppdtbb/custom/GPClassLoader.java
@@ -1,4 +1,59 @@
 package com.ppdtbb.custom;
 
-public class GPClassLoader {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class GPClassLoader extends ClassLoader {
+
+    private File baseDir;
+
+    public GPClassLoader() {
+        String basePath = GPClassLoader.class.getResource("").getPath();
+        this.baseDir = new File(basePath);
+    }
+
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+
+        String className = GPClassLoader.class.getPackage().getName() + "." + name;
+        if(baseDir != null) {
+            File classFile = new File(baseDir, name.replaceAll("\\.", "/") + ".class");
+            if(classFile.exists()) {
+                FileInputStream in = null;
+                ByteArrayOutputStream out = null;
+                try {
+                    in = new FileInputStream(classFile);
+                    out = new ByteArrayOutputStream();
+                    byte[] buff = new byte[1024];
+                    int len;
+                    while ((len=in.read(buff))!=-1) {
+                        out.write(buff, 0, len);
+                    }
+                    return defineClass(className, out.toByteArray(), 0, out.size());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if(null!=in) {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if(null!=out) {
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    classFile.delete();
+                }
+            }
+        }
+
+        return null;
+    }
+
 }
--- eduvipproxy/src/main/java/com/ppdtbb/custom/GPProxy.java
+++ eduvipproxy/src/main/java/com/ppdtbb/custom/GPProxy.java
@@ -1,5 +1,11 @@
 package com.ppdtbb.custom;
 
+import javax.tools.JavaCompiler;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 /**
@@ -15,12 +21,33 @@ public class GPProxy {
         String proxySrc = generateSrc(interfaces);
 
         //2、将生成的源代码输出到磁盘，保存为.java文件
+        try {
+            String filePath = GPProxy.class.getResource("").getPath();
+            File f = new File(filePath + "$Proxy0.java");
+            FileWriter fw = new FileWriter(f);
+            fw.write(proxySrc);
+            fw.flush();
+            fw.close();
 
-        //3、编译源代码，并且生成.class文件
+            //3、编译源代码，并且生成.class文件
+            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+            StandardJavaFileManager manager = compiler.getStandardFileManager(null, null, null);
+            Iterable iterable = manager.getJavaFileObjects(f);
 
-        //4、将class文件中的内容，动态加载到JVM中来
+            JavaCompiler.CompilationTask task = compiler.getTask(null, manager, null, null, null, iterable);
+            task.call();
+            manager.close();
 
-        //5、返回被代理后的代理对象
+            //4、将class文件中的内容，动态加载到JVM中来
+
+
+            //5、返回被代理后的代理对象
+            Class proxyClass = classLoader.findClass("$Proxy0");
+            Constructor c = proxyClass.getConstructor(GPInvocationHandler.class);
+            f.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
@@ -36,13 +63,20 @@ public class GPProxy {
         src.append("public $Proxy0(GPInvocationHandler h) {" + ln);
         src.append("this.h = h;" + ln);
         for(Method m : interfaces.getMethods()) {
-            src.append("public " + m.getReturnType() + " " + m.getName());
+            src.append("public " + m.getReturnType() + " " + m.getName() + "() throws Throwable {");
+            src.append("try {" + ln);
+            src.append("Method m = " + interfaces.getName() + ".class.getMethod(\"" + m.getName() + "\", new Class[]{})");
+            src.append("this.h.invoke(this, m, null);" + ln);
+            src.append("} catch(Exception e) {" + ln);
+            src.append("e.printStackTrace();");
+            src.append("}" + ln);
+            src.append("}" + ln);
         }
         src.append("}" + ln);
 
         src.append("}");
 
-        return "";
+        return src.toString();
     }
 
 }
